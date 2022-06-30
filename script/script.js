79bb93c58fdbc8a59ca861bc6781f617879843ae
Anna Usova <aptmos2016@yandex.ru>
2022-06-30 23:23:45 +0300

    Proekt Mesto s rabotaushim JS

--- script/script.js
+++ script/script.js
@@ -1,21 +1,49 @@
-const popup = document.querySelector('.popup');
+//кнопки открытия
 const openPopupButton = document.querySelector('.profile__edit-button');
-const closePopupButton = popup.querySelector('popup__button-close');
+const editProfileTitle = document.querySelector('.profile__title');
+const editProfileSubtitle = document.querySelector('.profile__subtitle');
+const popup = document.querySelector('.popup');
+const closePopupButton = popup.querySelector('.popup__button-close');
+const popupButtonSubmit = popup.querySelector('.popup__button-save');
+const popupName = popup.querySelector('.popup__name');
+const popupJob = popup.querySelector('.popup__job');
+const formElement = popup.querySelector('.popup__form-container');
 
-function popupOpenToggle() {
-  popup.classList.toggle('popup_opened');
+
+//функция открытия с возможностью записывать новые данные в форму
+function openPopup() {
+  popup.classList.add('popup_opened');
+  popupName.value = editProfileTitle.textContent;
+  popupJob.value = editProfileSubtitle.textContent;
 }
 
-function popupOverlayClickHandler(evt) {
-  //console.log(evt.target);
-  //console.log(evt.currentTarget);
-  if (evt.target === evt.currentTarget) {
-    popupOpenToggle();
-  }
+//функция закрытия попапа
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
 
-openPopupButton.addEventListener('click', popupOpenToggle);
-closePopupButton.addEventListener('click', popupOpenToggle);
-popup.addEventListener('click', popupOverlayClickHandler);
+
+// функция сохранения
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  editProfileTitle.textContent = popupName.value;
+  editProfileSubtitle.textContent = popupJob.value;
+  closePopup();
+}
+
+
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+
+
+
+
+
 
 
