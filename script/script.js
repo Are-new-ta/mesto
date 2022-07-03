75ff6e186e4031a23ec00d096e95197e87c2fdd0
Anna Usova <aptmos2016@yandex.ru>
2022-07-04 00:40:46 +0300

    После комментариев ревьюера

--- script/script.js
+++ script/script.js
@@ -1,20 +1,18 @@
-//кнопки открытия
+//переменные
 const openPopupButton = document.querySelector('.profile__edit-button');
 const editProfileTitle = document.querySelector('.profile__title');
 const editProfileSubtitle = document.querySelector('.profile__subtitle');
 const popup = document.querySelector('.popup');
 const closePopupButton = popup.querySelector('.popup__button-close');
-const popupButtonSubmit = popup.querySelector('.popup__button-save');
-const popupName = popup.querySelector('.popup__name');
-const popupJob = popup.querySelector('.popup__job');
+const popupInputName = popup.querySelector('.popup__input_data_name');
+const popupInputJob = popup.querySelector('.popup__input_data_job');
 const formElement = popup.querySelector('.popup__form-container');
-
 
 //функция открытия с возможностью записывать новые данные в форму
 function openPopup() {
-  popup.classList.add('popup_opened');
   popupName.value = editProfileTitle.textContent;
   popupJob.value = editProfileSubtitle.textContent;
+  popup.classList.add('popup_opened');
 }
 
 //функция закрытия попапа
@@ -22,15 +20,13 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-
 // функция сохранения
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  editProfileTitle.textContent = popupName.value;
-  editProfileSubtitle.textContent = popupJob.value;
+  editProfileTitle.textContent = popupInputName.value;
+  editProfileSubtitle.textContent = popupInputJob.value;
   closePopup();
 }
-
 
 openPopupButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click', closePopup);
