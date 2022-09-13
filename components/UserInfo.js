eb36dd6847d0a0491cd3c9df1c804c482a5df819
Anna Usova <aptmos2016@yandex.ru>
2022-09-13 23:45:00 +0300

    fix(js)поля в попап профиль присваивают title/subtitle

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -14,11 +14,50 @@ export default class UserInfo {
     return this._user;
   }
 
-  setUserInfo(newName, newJob) {
-    this._name.textContent = newName;
-    this._job.textContent = newJob;
+  setUserInfo(name, job) {
+    this._name.textContent = name;
+    this._job.textContent = job;
   }
 }
+
+
+
+//последняя версия
+// export default class UserInfo {
+//   constructor(selectorName, selectorJob) {
+//     this._selectorName = selectorName;
+//     this._selectorJob = selectorJob;
+//     this._name = document.querySelector(this._selectorName);
+//     this._job = document.querySelector(this._selectorJob);
+//   }
+
+//   getUserInfo() {
+//     this._user = {
+//       name: this._name.textContent,
+//       job: this._job.textContent
+//     };
+//     return this._user;
+//   }
+
+//   setUserInfo(data) {
+//   this._name.textContent = data.name;
+// this._job.textContent = jdata.ob;
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // export default class UserInfo {
